39646630b0aec574547cc00be4538e3fe3126352
ningchang <754167208@qq.com>
2022-04-25 16:00:27 +0800

    工程初始化

---
+++ apigateway/src/main/java/cn/chennian/apigateway/ApigatewayApplication.java
@@ -0,0 +1,14 @@
+package cn.chennian.apigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+public class ApigatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApigatewayApplication.class, args);
+    }
+
+}
