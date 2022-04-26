08e9963da8ce2cb7be445847024bf2410e7eb75d
ningchang <754167208@qq.com>
2022-04-26 14:59:30 +0800

    读取nacos 服务

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # spring-cloud-knowledge
+
+# nacos ：nacos 2.0 版本以后，docker 启动时除了8848 启动端口还需要开放 9848 9849 两个端口
--- apigateway/src/main/java/cn/chennian/apigateway/ApigatewayApplication.java
+++ apigateway/src/main/java/cn/chennian/apigateway/ApigatewayApplication.java
@@ -2,13 +2,20 @@ package cn.chennian.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@RefreshScope
 public class ApigatewayApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ApigatewayApplication.class, args);
+        ConfigurableApplicationContext applicationContext = SpringApplication.run(ApigatewayApplication.class, args);
+        String userName = applicationContext.getEnvironment().getProperty("user.age");
+        System.out.println(userName);
     }
 
 }
